4adb5d44b008fd9d88d20d99b09d08e2aadaf03a
Edward <pryoreb197@potsdam.edu>
2016-09-30 06:23:54 -0400

    rake method implemend. loop and get colors dummied out

--- init.py
+++ init.py
@@ -27,7 +27,8 @@ class cyclopz:
         self.k_default_shoulder = 1500
         self.k_default_elbow = 1500
         self.k_default_wrist = 1500
-        self.k_default_hand = 2500
+        self.k_default_hand = 1500
+        self.count = 1
 
         try:
             self.usb.open()
@@ -52,11 +53,107 @@ class cyclopz:
         self.usb.write("#0 P"+str(self.k_pink_pile)+"  S"+str(self.k_default_speed)+" \r")
         time.sleep(6)
         self.usb.write("#0 P"+str(self.k_trash_pile)+" S"+str(self.k_default_speed)+" \r")
+        time.sleep(6)
+        self.usb.write("#0 P"+str(self.k_red_pile)+"  S"+str(self.k_default_speed)+" \r")
+        time.sleep(6)
     def terminate(self):
         self.usb.close()
+
+    def move_to_trash(self):
+        self.usb.write("#1 P"+str(self.k_default_shoulder)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#2 P"+str(self.k_default_elbow)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#0 P"+str(self.k_trash_pile)+" S"+str(self.k_default_speed)+" \r") 
+        time.sleep(3)
+        self.usb.write("#1 P900 S"+str(self.k_default_speed)+" \r")
+        time.sleep(1)
+        self.usb.write("#3 P1000 S"+str(self.k_default_speed)+" \r") 
+        time.sleep(1)
+    def move_to_red(self):
+        self.usb.write("#1 P"+str(self.k_default_shoulder)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#2 P"+str(self.k_default_elbow)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#0 P"+str(self.k_red_pile)+" S"+str(self.k_default_speed)+" \r") 
+        time.sleep(3)
+        self.usb.write("#1 P1300 S"+str(self.k_default_speed)+" \r")
+        time.sleep(1)
+        self.usb.write("#3 P900 S"+str(self.k_default_speed)+" \r") 
+        time.sleep(1)
+    def move_to_yellow(self):
+        self.usb.write("#1 P"+str(self.k_default_shoulder)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#2 P"+str(self.k_default_elbow)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#0 P"+str(self.k_yellow_pile)+" S"+str(self.k_default_speed)+" \r") 
+        time.sleep(3)
+        self.usb.write("#1 P1300 S"+str(self.k_default_speed)+" \r")
+        time.sleep(1)
+        self.usb.write("#3 P900 S"+str(self.k_default_speed)+" \r") 
+        time.sleep(1)
+    def move_to_pink(self):
+        self.usb.write("#1 P"+str(self.k_default_shoulder)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#2 P"+str(self.k_default_elbow)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#0 P"+str(self.k_pink_pile)+" S"+str(self.k_default_speed)+" \r") 
+        time.sleep(3)
+        self.usb.write("#1 P1300 S"+str(self.k_default_speed)+" \r")
+        time.sleep(1)
+        self.usb.write("#3 P900 S"+str(self.k_default_speed)+" \r") 
+        time.sleep(1)
+    def move_to_home(self):
+        self.usb.write("#1 P"+str(self.k_default_shoulder)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#2 P"+str(self.k_default_elbow)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#0 P"+str(self.k_pile_pile)+" S"+str(self.k_default_speed)+" \r") 
+        self.usb.write("#4 P"+str(self.k_default_hand)+"  S"+str(self.k_default_speed)+" \r")
+        time.sleep(2)
+    def rake(self):
+        #line up arm wit pile
+        self.usb.write("#0 P"+str(self.k_pile_pile)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#1 P1025 S200 \r")
+        self.usb.write("#2 p1500 S200 \r")
+        time.sleep(1)
+        #dive te arm into the pile
+        self.usb.write("#3 p1000 S200 \r")
+        self.usb.write("#4 p1000 S300 \r")
+        time.sleep(3)
+        #draw arm back through pile
+        i = 1025
+        j = 1500
+        while not self.test_image() and i <= 2000 and j <= 2500:
+            self.usb.write("#1 P"+str(i+7)+" S100 \r")
+            self.usb.write("#2 P"+str(j+26)+" S125 \r")
+            time.sleep(.1)
+            i = i+5
+            j = j+10
+        self.usb.write("#4 p7500 S300 \r")
+        time.sleep(3)
+    
+    def rotLeft(self):
+        self.usb.write("#0 P"+str(self.k_pile_pile-self.count*20)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#2 p1500 S200 \r")
+        self.count=self.count+1
+        time.sleep(4)
+    def rotRight(self):
+        self.usb.write("#0 P"+str(self.k_pile_pile+self.count*20)+" S"+str(self.k_default_speed)+" \r")
+        self.usb.write("#2 p1500 S200 \r")
+        self.count=self.count+1
+        time.sleep(4)
+    def test_image(self):
+        pass
+        return False
 
 print("hear we go")
 a = cyclopz()
 time.sleep(6)
-a.test()
-
+#a.test()
+#time.sleep(2)
+#a.move_to_trash()
+#time.sleep(3)
+#a.move_to_red()
+#time.sleep(3)
+#a.move_to_yellow()
+#time.sleep(3)
+#a.move_to_pink()
+while a.count < 15:
+    a.move_to_home()
+    a.rake()
+    a.move_to_home()
+    a.rotLeft()
+    a.rake()
+    a.move_to_home()
+    a.rotRight()
