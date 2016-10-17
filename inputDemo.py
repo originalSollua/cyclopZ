431cb3e94ca65112a09ae7a0ad46a98eeacbe797
Pi Pierson <achicks@us.ibm.com>
2016-10-17 18:37:49 +0000

    global fix

--- inputDemo.py
+++ inputDemo.py
@@ -38,7 +38,8 @@ def handler_exit(button):
 def handler_open(button):
     c.open()
 def quit():
-   global exit = True
+    global exit
+    exit = True
 prevx = 0
 prevy = 0
 controler.register_button_handler(handler_close, SixAxis.button_circle)
